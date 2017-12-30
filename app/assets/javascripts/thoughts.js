bc72346f90548fa84e7c8d2bcfac0d91b2ff07b8
leog7one <lguzman@me.com>
2017-12-29 19:45:03 -0600

    started commemt creation using json backend, need to have commet load correctly

--- app/assets/javascripts/thoughts.js
+++ app/assets/javascripts/thoughts.js
@@ -22,19 +22,52 @@ document.addEventListener("turbolinks:load", function() {
 
 
   //CREATE COMMENTS VIA AJAX
-  $('#new_comment').unbind("submit").bind("submit", function(e) {
-    $.ajax({
-      type: "POST",
-      url: this.action,
-      data: $(this).serialize(),
-      success: function(response) {
-        $("#comment_body").val("");
-        var $ol = $("div.thought-comments").html(response)
-      }
-    });
-    e.preventDefault();
-  })
+  // $('#new_comment').unbind("submit").bind("submit", function(e) {
+  //   $.ajax({
+  //     type: "POST",
+  //     url: this.action,
+  //     data: $(this).serialize(),
+  //     success: function(response) {
+  //       $("#comment_body").val("");
+  //       var $ol = $("div.thought-comments").html(response)
+  //     }
+  //   });
+  //   e.preventDefault();
+  // })
 
+  // JS Constructor - Comment
+function Comment(comment) {
+  this.id = comment.id
+  this.username = comment.thinker.username
+  this.comment = comment.body
+}
+
+// Prototype method
+  Comment.prototype.formatComment = function() {
+    commentHTML = `<li data-id=${this.id}><b>On ${this.username}:</b> ${this.comment} </li>`
+    return commentHTML
+  }
+
+  //Submit comment using JSON and RAILS API
+
+    $(".new_comment").on("submit", function(e){
+      $.post(this.action, $(this).serialize(), function(comment) {
+        if (Array.isArray(comment)) {
+          var message = "";
+          comment.forEach(function(error) {
+            message += `${error}\n`
+          })
+          alert(message);
+        } else {
+          const $ol = $("div.thought-comments");
+          const newComment = new Comment(comment);
+          const commentHTML = newComment.formatComment();
+          $ol.append(commentHTML);
+        }
+        $("div.thought-comments").val("");
+      });
+      e.preventDefault();
+      })
 
 })
 
